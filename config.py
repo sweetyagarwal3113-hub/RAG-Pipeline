34a52c19c5dbd2518635ed347826fd82247b67ef
sweetyagarwal3113-hub <sweetyagarwl3113@gmail.com>
2026-07-13 19:19:54 +0530

    config

--- config.py
+++ config.py
@@ -9,9 +9,11 @@ if not GOOGLE_API_KEY:
     try:
         import streamlit as st
         GOOGLE_API_KEY = st.secrets["GOOGLE_API_KEY"]
-    except Exception:
-        pass
+    except KeyError:
+        raise ValueError("GOOGLE_API_KEY not found in Streamlit Secrets! Please add it in Advanced Settings.")
 
+if GOOGLE_API_KEY:
+    os.environ["GOOGLE_API_KEY"] = GOOGLE_API_KEY
 CHROMA_PATH = "db/chroma_db"
 
 PDF_PATH = "data/Demo.pdf"
