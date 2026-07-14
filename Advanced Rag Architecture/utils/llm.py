5f68eeffc647a2dd0e37c95c1f1405f4a98b8478
sweetyagarwal3113-hub <sweetyagarwl3113@gmail.com>
2026-07-14 13:38:20 +0530

    llm

--- Advanced Rag Architecture/utils/llm.py
+++ Advanced Rag Architecture/utils/llm.py
@@ -3,7 +3,7 @@ from config import GROQ_API_KEY
 
 def load_llm():
     llm = ChatGroq(
-        model="llama3-8b-8192",
+        model="llama-3.1-8b-instant",
         api_key=GROQ_API_KEY,
         temperature=0
     )
