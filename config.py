707aaf0e9060053f2fa9693d461b11da802443fa
sweetyagarwal3113-hub <sweetyagarwl3113@gmail.com>
2026-07-14 10:50:26 +0530

    revert gemini 3.5 flash

--- config.py
+++ config.py
@@ -13,15 +13,6 @@ if not GOOGLE_API_KEY:
 if GOOGLE_API_KEY:
     os.environ["GOOGLE_API_KEY"] = GOOGLE_API_KEY
 
-HF_TOKEN = os.getenv("HF_TOKEN")
-if not HF_TOKEN:
-    try:
-        import streamlit as st
-        HF_TOKEN = st.secrets["HF_TOKEN"]
-    except KeyError:
-        raise ValueError("HF_TOKEN not found in Streamlit Secrets! Please add it in Advanced Settings.")
-if HF_TOKEN:
-    os.environ["HF_TOKEN"] = HF_TOKEN
 CHROMA_PATH = "db/chroma_db"
 
 PDF_PATH = "data/Demo.pdf"
--- utils/llm.py
+++ utils/llm.py
@@ -1,10 +1,10 @@
-from langchain_huggingface import HuggingFaceEndpoint
-from config import HF_TOKEN
+from langchain_google_genai import ChatGoogleGenerativeAI
+from config import GOOGLE_API_KEY
 
 def load_llm():
-    llm = HuggingFaceEndpoint(
-        repo_id="HuggingFaceH4/zephyr-7b-beta",
-        temperature=0.1,
-        huggingfacehub_api_token=HF_TOKEN
+    llm = ChatGoogleGenerativeAI(
+        model="gemini-3.5-flash",
+        google_api_key=GOOGLE_API_KEY,
+        temperature=0
     )
     return llm
